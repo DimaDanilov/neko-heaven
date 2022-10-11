cec57bd60dccc431c15d1d14e4cc9b0bdd08f538
Dima Danilov <dima.danilov.2012@mail.ru>
2022-10-11 13:33:35 +0300

    scroll on wheel

--- src/components/CatSlider/CatSlider.tsx
+++ src/components/CatSlider/CatSlider.tsx
@@ -1,10 +1,10 @@
 import { useLayoutEffect, useRef, useState, FC } from "react";
 import styled from 'styled-components';
-import arrowImg from '../../assets/icons/arrow-right.svg';
 import { SliderItem } from "./SliderItem"
 
-export const CatSlider: FC = () => {
+const scrollMultiplier = 0.5;
 
+export const CatSlider: FC = () => {
     const divRef = useRef<HTMLDivElement>(null);
     const [width, setWidth] = useState(0); // Width of container to move slider on width of container
 
@@ -14,16 +14,18 @@ export const CatSlider: FC = () => {
             setWidth(divRef.current.offsetWidth);
     }, []);
 
-    const moveSlider = (width: number) => {
+    const moveSlider = (e: React.WheelEvent<HTMLDivElement>, width: number) => {
         if (divRef.current)
-            divRef.current.scrollLeft += width;
+            if (e.deltaY > 0)
+                divRef.current.scrollLeft += width * scrollMultiplier
+            else
+                divRef.current.scrollLeft -= width * scrollMultiplier
+        // console.log(e.deltaY)
     }
 
     return (
         <SliderContainer>
-            <PreBtn onClick={() => moveSlider(-width)}><img src={arrowImg} alt="" /></PreBtn>
-            <NxtBtn onClick={() => moveSlider(width)}><img src={arrowImg} alt="" /></NxtBtn>
-            <ImgContainer ref={divRef}>
+            <ImgContainer ref={divRef} onWheel={(e) => moveSlider(e, width)}>
                 <SliderItem />
                 <SliderItem />
                 <SliderItem />
@@ -34,40 +36,12 @@ export const CatSlider: FC = () => {
     )
 };
 
-
 const SliderContainer = styled.section`
     height: calc(100vh - 50px);
     padding: 20px;
     position: relative;
     overflow: hidden;
     box-sizing: border-box;
-`
-const PreBtn = styled.button`
-    border: none;
-    width: 10vw;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #fff 100%);
-    cursor: pointer;
-    left: 0;
-    transform: rotate(180deg);
-`
-const NxtBtn = styled.button`
-    border: none;
-    width: 10vw;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #fff 100%);
-    cursor: pointer;
-    right: 0;
 `
 const ImgContainer = styled.div`
     padding: 0 10vw;
