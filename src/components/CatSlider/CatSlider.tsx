f8192b69835271f3d4dbfd6e25fb2873280a68a6
Dima Danilov <dima.danilov.2012@mail.ru>
2022-10-12 14:51:14 +0300

    size slider feature: v1

--- src/components/CatSlider/CatSlider.tsx
+++ src/components/CatSlider/CatSlider.tsx
@@ -1,12 +1,23 @@
 import { useLayoutEffect, useRef, useState, FC } from "react";
 import styled from 'styled-components';
 import { SliderItem } from "./SliderItem"
+import oneImg from '../../assets/images/1.png';
+import twoImg from '../../assets/images/2.png';
+import threeImg from '../../assets/images/3.png';
+import fourImg from '../../assets/images/4.png';
+import fiveImg from '../../assets/images/5.png';
+import sixImg from '../../assets/images/6.png';
+import sevenImg from '../../assets/images/7.png';
+import eightImg from '../../assets/images/8.png';
 
-const scrollMultiplier = 0.5;
+
+const scrollMultiplier = 0.2;
+const screenWidth = window.innerWidth
 
 export const CatSlider: FC = () => {
     const divRef = useRef<HTMLDivElement>(null);
     const [width, setWidth] = useState(0); // Width of container to move slider on width of container
+    const [currentCenter, setCurrentCenter] = useState(screenWidth / 2)
 
     // Find width of container to move slider
     useLayoutEffect(() => {
@@ -14,25 +25,34 @@ export const CatSlider: FC = () => {
             setWidth(divRef.current.offsetWidth);
     }, []);
 
-    const moveSlider = (e: React.WheelEvent<HTMLDivElement>, width: number) => {
-        if (divRef.current)
+    const wheelHandler = (e: React.WheelEvent<HTMLDivElement>, width: number) => {
+        if (divRef.current) {
             if (e.deltaY > 0)
                 divRef.current.scrollLeft += width * scrollMultiplier
             else
                 divRef.current.scrollLeft -= width * scrollMultiplier
-        // console.log(e.deltaY)
+            console.log(divRef.current.scrollLeft + screenWidth / 2)
+        }
+    }
+
+    const scrollHandle = (e: React.UIEvent<HTMLDivElement>) => {
+        if (divRef.current)
+            setCurrentCenter(divRef.current.scrollLeft + screenWidth / 2) // Count center of Slider again
     }
 
     return (
         <SliderContainer>
-            <ImgContainer ref={divRef} onWheel={(e) => moveSlider(e, width)}>
-                <SliderItem />
-                <SliderItem />
-                <SliderItem />
-                <SliderItem />
-                <SliderItem />
+            <ImgContainer ref={divRef} onWheel={(e) => wheelHandler(e, width)} onScroll={(e) => scrollHandle(e)} >
+                <SliderItem imgSrc={oneImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={twoImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={threeImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={fourImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={fiveImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={sixImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={sevenImg} windowWidth={screenWidth} currentCenter={currentCenter} />
+                <SliderItem imgSrc={eightImg} windowWidth={screenWidth} currentCenter={currentCenter} />
             </ImgContainer>
-        </SliderContainer >
+        </SliderContainer>
     )
 };
 
@@ -48,7 +68,7 @@ const ImgContainer = styled.div`
     display: flex;
     overflow-x: auto;
     scroll-behavior: smooth;
-    gap: 10%;
+    gap: 8%;
     height: 100%;
     &::-webkit-scrollbar {
         display: none;
--- src/components/CatSlider/SliderItem.tsx
+++ src/components/CatSlider/SliderItem.tsx
@@ -1,10 +1,47 @@
-import { FC } from "react";
+import { FC, useLayoutEffect, useRef, useState } from "react";
 import styled from 'styled-components';
-import stockImage from '../../assets/images/stock-image.png';
 
-export const SliderItem: FC = () => <SliderImage src={stockImage} />
+interface SliderImageProps {
+    mymy: string
+}
 
-const SliderImage = styled.img`
+export interface SliderItemProps {
+    imgSrc: string,
+    windowWidth: number,
+    currentCenter: number
+}
+
+const maxHeightPercent = 90
+
+export const SliderItem = ({ imgSrc, windowWidth, currentCenter }: SliderItemProps) => {
+
+    const imgRef = useRef<HTMLImageElement>(null);
+    const [imageHeight, setImageHeight] = useState('0%');
+
+    useLayoutEffect(() => {
+        if (imgRef.current) {
+            // let imgPosition = imgRef.current.offsetLeft;
+            let imgPosition = imgRef.current.offsetLeft + (imgRef.current.width / 2);
+            let newImageHeight = -(((imgPosition - currentCenter) ** 2) / (((windowWidth / 2) / 5) ** 2)) + maxHeightPercent
+            if (newImageHeight > 0) {
+                setImageHeight(newImageHeight + '%');
+            } else if (imgPosition < currentCenter) {
+                // If img size should be lower than 0% then smooth size change
+                let newImageHeight = -(((imgPosition - currentCenter + (windowWidth / 2) * 20) ** 2) / (((windowWidth / 2) / 5) ** 2)) + maxHeightPercent
+                setImageHeight(newImageHeight + '%')
+            }
+            else {
+                let newImageHeight = -(((imgPosition - currentCenter - (windowWidth / 2) * 20) ** 2) / (((windowWidth / 2) / 5) ** 2)) + maxHeightPercent
+                setImageHeight(newImageHeight + '%')
+            }
+            console.log(currentCenter)
+        }
+    }, [currentCenter, imageHeight]);
+
+    return <SliderImage ref={imgRef} src={imgSrc} mymy={imageHeight} />
+}
+
+const SliderImage = styled.img<SliderImageProps>`
     alt: "";
-    height: 100%;
+    height: ${props => props.mymy};
 `
